752bfd4647865e5d8388621c8b0951dfbcdd78e8
asifanwar1 <asifanwar0705@gmail.com>
2026-05-11 13:13:01 +0500

    path setup

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="Pulse Store API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:1337"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
